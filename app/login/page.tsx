f63a989fc523c8eb86bcb85f9dbaf1d33e71772c
Hendra <etik.hendra@gmail.com>
2025-10-31 06:42:39 +0700

    add the remember me

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,7 +12,7 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="w-screen min-h-screen flex flex-col items-center justify-center bg-[#ffffff] px-4">
+    <div className="w-screen min-h-screen fixed flex flex-col items-center justify-center bg-[#ffffff] px-4">
       <div className="mb-10">
         <h1
           className=" text-[56px] font-semibold text-center mb-8 text-[#44444F]"
@@ -27,9 +27,9 @@ export default function LoginPage() {
           Just sign in if you have an account in here. Enjoy our Website
         </h2>
       </div>
-      <div className="w-[560px] h-[314px] bg-gray rounded-2xl shadow-lg p-8">
+      <div className="w-[560px] h-[314px] bg-gray rounded-[20px] shadow-sm p-8">
         <form onSubmit={handleSubmit} className="space-y-4">
-          <div className="relative mt-6">
+          <div className="relative mt-3">
             <label className="absolute -top-2 ml-[15px] w-[133px] bg-[#ffffff] text-center block text-[12px] font-normal text-[#50B5FF] mb-1">
               Your Email / Username
             </label>
@@ -43,7 +43,7 @@ export default function LoginPage() {
             />
           </div>
 
-          <div className="relative mt-[16]">
+          <div className="relative mt-6">
             <label className="absolute -top-2 ml-[15px] w-16 bg-[#ffffff] text-center block text-[12px] font-normal text-[#FC5A5A] mb-1">
               Password
             </label>
@@ -56,23 +56,28 @@ export default function LoginPage() {
               required
             />
           </div>
+          <label className="flex items-center space-x-2 cursor-pointer">
+            <input
+              type="checkbox"
+              className="h-4 w-4 accent-[#50B5FF] cursor-pointer text-white"
+            />
+            <span className="text-[14px] text-[#696974]">Remember me</span>
+          </label>
 
           <button
             type="submit"
-            className="cursor-pointer w-full bg-[#0062FF] text-white py-2 rounded-lg font-medium hover:bg-[#044dc3] transition"
+            className="cursor-pointer w-[500px] h-12 bg-[#0062FF] text-white rounded-[10px] font-semibold text-[12px] hover:bg-[#044dc3] transition"
           >
-            Sign In
+            Login
           </button>
         </form>
-
-       
       </div>
-       <p className="text-center text-sm text-gray-600 mt-4">
-          Don’t have an account?{" "}
-          <a href="/register" className="text-blue-600 hover:underline">
-            Register
-          </a>
-        </p>
+      <p className="text-center font-medium text-[14px] text-[#0062FF] mt-[15px]">
+        Don’t have an account?{" "}
+        <a href="/register" className="text-blue-600 hover:underline">
+          Register
+        </a>
+      </p>
     </div>
   );
 }
