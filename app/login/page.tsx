b55a68a6f983e0f4036371d1a6e87daab878bfaa
Hendra <etik.hendra@gmail.com>
2025-10-31 06:19:08 +0700

    change the style

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,30 +12,30 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 px-4">
-      <div className="mb-15">
+    <div className="w-screen min-h-screen flex flex-col items-center justify-center bg-[#ffffff] px-4">
+      <div className="mb-10">
         <h1
-          className="text-[56px] font-semibold text-center mb-6 text-[#44444F]"
+          className=" text-[56px] font-semibold text-center mb-8 text-[#44444F]"
           style={{ fontFamily: "Poppins, sans-serif" }}
         >
           Sign In
         </h1>
         <h2
-          className="text-[16px] font-normal text-center  text-[#44444F]"
+          className="w-[438px] text-[16px] font-normal text-center  text-[#44444F]"
           style={{ fontFamily: "Poppins, sans-serif" }}
         >
           Just sign in if you have an account in here. Enjoy our Website
         </h2>
       </div>
-      <div className="w-full max-w-sm bg-white rounded-2xl shadow-lg p-8">
+      <div className="w-[560px] h-[314px] bg-gray rounded-2xl shadow-lg p-8">
         <form onSubmit={handleSubmit} className="space-y-4">
-          <div>
-            <label className="block text-sm font-medium text-gray-700 mb-1">
-              Email
+          <div className="relative mt-6">
+            <label className="absolute -top-2 ml-[15px] w-[133px] bg-[#ffffff] text-center block text-[12px] font-normal text-[#50B5FF] mb-1">
+              Your Email / Username
             </label>
             <input
               type="email"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+              className="w-[500px] h-[48] text-[#44444F] pl-[15px] border border-[#50B5FF] rounded-[10px] focus:outline-none focus:ring-1 focus:ring-[#3bacfd]"
               placeholder="Enter your email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
@@ -43,13 +43,13 @@ export default function LoginPage() {
             />
           </div>
 
-          <div>
-            <label className="block text-sm font-medium text-gray-700 mb-1">
+          <div className="relative mt-[16]">
+            <label className="absolute -top-2 ml-[15px] w-16 bg-[#ffffff] text-center block text-[12px] font-normal text-[#FC5A5A] mb-1">
               Password
             </label>
             <input
               type="password"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+              className="w-[500px] h-[48] text-[#44444F] pl-[15px] border border-[#FC5A5A] rounded-[10px] focus:outline-none focus:ring-1 focus:ring-[#fe4f4f]"
               placeholder="Enter your password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
@@ -59,19 +59,20 @@ export default function LoginPage() {
 
           <button
             type="submit"
-            className="w-full bg-black text-white py-2 rounded-lg font-medium hover:bg-gray-800 transition"
+            className="cursor-pointer w-full bg-[#0062FF] text-white py-2 rounded-lg font-medium hover:bg-[#044dc3] transition"
           >
             Sign In
           </button>
         </form>
 
-        <p className="text-center text-sm text-gray-600 mt-4">
+       
+      </div>
+       <p className="text-center text-sm text-gray-600 mt-4">
           Don’t have an account?{" "}
           <a href="/register" className="text-blue-600 hover:underline">
             Register
           </a>
         </p>
-      </div>
     </div>
   );
 }
