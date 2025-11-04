5e70b6744e2145545c2cb28255b0748085bb9cbf
Hendra <etik.hendra@gmail.com>
2025-11-04 21:56:08 +0700

    start building the password

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -14,6 +14,7 @@ export default function RegisterPage() {
     confirmPassword: "",
     about: "",
   });
+  const [showPassword,setShowPassword] = useState(false)
 
   const handleChange = (
     e: React.ChangeEvent<
@@ -76,7 +77,7 @@ export default function RegisterPage() {
                 required
               >
                 <option value="+62">+62</option>
-                <option value="+1">+1</option>
+                <option value="+60">+60</option>
                 <option value="+91">+91</option>
               </select>
               <input
@@ -119,14 +120,19 @@ export default function RegisterPage() {
           </div>
 
           <div className="flex flex-row justify-between mt-6">
-            <input
-              type="password"
-              className="w-[500px] h-[48] placeholder:text-[#B5B5BE] text-[#44444F] pl-[15px] border border-[#E2E2EA] rounded-[10px]  focus:outline-none focus:ring-1 focus:ring-[#3bacfd]"
-              placeholder="Password"
-              value={formdata.password}
-              onChange={handleChange}
-              required
-            />
+            <div className="relative flex">
+              <input
+                type="password"
+                className="w-60 h-12 placeholder:text-[#B5B5BE] text-[#44444F] pl-[15px] border border-[#E2E2EA] rounded-[10px]  focus:outline-none focus:ring-1 focus:ring-[#3bacfd]"
+                placeholder="Password"
+                value={formdata.password}
+                onChange={handleChange}
+                required
+              />
+              <button className="absolute  top-4 right-4 ">
+                tes
+              </button>
+            </div>
           </div>
 
           <button
